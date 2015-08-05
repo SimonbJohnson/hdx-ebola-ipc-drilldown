c0c74143d4f8edff540329a0a6db6e700c89795d
SJohnson <simonbjohnson@gmail.com>
2015-08-05 16:56:43 +0100

    second iteration

--- js/site.js
+++ js/site.js
@@ -55,10 +55,9 @@ function generate3WComponent(data,geom,map,key){
 
     dc.chartRegistry.clear();
     $('#hdx-ipc-who').html('<p>Who | Current filter: <span class="filter"></span></span></p>');
-    $('#hdx-ipc-status').html('<p>What | Current filter: <span class="filter"></span></span></p>');
-    $('#hdx-ipc-type').html('<p>Status | Current filter: <span class="filter"></span></span></p>');
-    $('#hdx-ipc-duration').html('<p>Declared at district level | Current filter: <span class="filter"></span></span></p>');
-    $('#hdx-ipc-date').html('<p>Declared at district level | Current filter: <span class="filter"></span></span></p>');
+    $('#hdx-ipc-status').html('<p>Status | Current filter: <span class="filter"></span></span></p>');
+    $('#hdx-ipc-type').html('<p>Type | Current filter: <span class="filter"></span></span></p>');
+    $('#hdx-ipc-duration').html('<p>Duration | Current filter: <span class="filter"></span></span></p>');
     $('.hdx-3w-info').remove();
 
     //lookUpVDCCodeToName = genLookupVDCCodeToName(geom,config);
@@ -69,6 +68,7 @@ function generate3WComponent(data,geom,map,key){
     var durationChart = dc.rowChart('#hdx-ipc-duration');
     //var dateChart = dc.lineChart('#hdx-ipc-date');
     var whereChart = dc.leafletChoroplethChart('#hdx-ipc-where');
+    var whereBarChart = dc.rowChart('#hdx-ipc-wherebar');
 
     var cf = crossfilter(data);
 
@@ -99,44 +99,59 @@ function generate3WComponent(data,geom,map,key){
             return d[key]; 
         }});
 
+    var where2Dimension = cf.dimension(function(d){ if(d[key]==null){
+            return 'No Data';
+        } else {
+            return d[key]; 
+        }});    
+
     var whoGroup = whoDimension.group();
     var statusGroup = statusDimension.group();
     var typeGroup = typeDimension.group();
     var durationGroup = durationDimension.group();
     var whereGroup = whereDimension.group();
+    var where2Group = where2Dimension.group();
     var all = cf.groupAll();
 
-    whoChart.width($('#hdx-ipc-who').width()).height(400)
+    whoChart.width($('#hdx-ipc-who').width()).height(200)
             .dimension(whoDimension)
             .group(whoGroup)
             .elasticX(true)
+            .colors([color])
+            .colorAccessor(function(d, i){return 0;})
             .label(function(d){
                 return d.key +' ('+d.value+')';
             })            
             .xAxis().ticks(5);
 
-    statusChart.width($('#hdx-ipc-status').width()).height(300)
+    statusChart.width($('#hdx-ipc-status').width()).height(200)
             .dimension(statusDimension)
             .group(statusGroup)
             .elasticX(true)
+            .colors([color])
+            .colorAccessor(function(d, i){return 0;})
             .label(function(d){
                 return d.key +' ('+d.value+')';
             })
             .xAxis().ticks(5);    
     
-    typeChart.width($('#hdx-ipc-type').width()).height(300)
+    typeChart.width($('#hdx-ipc-type').width()).height(200)
             .dimension(typeDimension)
             .group(typeGroup)
             .elasticX(true)
+            .colors([color])
+            .colorAccessor(function(d, i){return 0;})
             .label(function(d){
                 return d.key +' ('+d.value+')';
             })
             .xAxis().ticks(5); 
 
-    durationChart.width($('#hdx-ipc-duration').width()).height(300)
+    durationChart.width($('#hdx-ipc-duration').width()).height(200)
             .dimension(durationDimension)
             .group(durationGroup)
             .elasticX(true)
+            .colors([color])
+            .colorAccessor(function(d, i){return 0;})
             .label(function(d){
                 return d.key +' ('+d.value+')';
             })
@@ -145,6 +160,21 @@ function generate3WComponent(data,geom,map,key){
     dc.dataCount('#count-info')
             .dimension(cf)
             .group(all);
+
+    whereBarChart.width($('#hdx-ipc-wherebar').width()).height(300)
+            .dimension(where2Dimension)
+            .group(where2Group)
+            .elasticX(true)
+            .colors([color])
+            .colorAccessor(function(d, i){return 0;})
+            .label(function(d){
+                if(d.key==''){
+                    return 'No Data ('+d.value+')';
+                } else {
+                    return lookupSmall[d.key] +' ('+d.value+')';
+                }
+            })
+            .xAxis().ticks(5);        
 
     whereChart.width($('#hdx-ipc-where').width()).height(300)
             .dimension(whereDimension)
@@ -177,49 +207,52 @@ function generate3WComponent(data,geom,map,key){
             .createLeaflet(function(){
                 return map;
             })
-            .on('filtered',function(e){
-                if(admlevel<3){
-                    var cf = crossfilter(data);
-                    map.removeLayer(dcGeoLayer);                    
-                }               
-                if(admlevel==1){      
-                    var overlay = L.geoJson(geom,{
-                            style:{
-                                fillColor: "#000000",
-                                color: 'grey',
-                                weight: 2,
-                                opacity: 1,
-                                fillOpacity: 0,
-                                class:'adm'+admlevel
-                            },
-                            onEachFeature: onEachFeatureADM1
-                    });
-                    overlay.addTo(map);                      
-                    var whereDimension = cf.dimension(function(d,i){return d['#adm1+code']; });
-                    var newData = whereDimension.filter(e.filters()[0]).top(Infinity);
-                    var newGeom = filterGeom(adm2_geom,e.filters()[0],6);
-                    overlay1 = overlay;
-                    admlevel=2;
-                    generate3WComponent(newData,newGeom,map,'#adm2+code');
-                } else if(admlevel==2){
-                    /*var overlay = L.geoJson(geom,{
-                            style:{
-                                fillColor: "#000000",
-                                color: 'grey',
-                                weight: 2,
-                                opacity: 1,
-                                fillOpacity: 0,
-                                class:'adm'+admlevel
-                            },
-                            onEachFeature: onEachFeatureADM2
-                    }); */                   
-                    var whereDimension = cf.dimension(function(d,i){return d['#adm2+code']; });
-                    var newData = whereDimension.filter(e.filters()[0]).top(Infinity);
-                    var newGeom = filterGeom(adm3_geom,e.filters()[0],9);
-                    /*overlay.addTo(map); 
-                    overlay2 = overlay;*/
-                    admlevel=3;
-                    generate3WComponent(newData,newGeom,map,'#adm3+code');
+            .on('filtered',function(chart){
+                var filters = chart.filters();
+                if(filters.length>0){
+                    if(admlevel<3){
+                        var cf = crossfilter(data);
+                        map.removeLayer(dcGeoLayer);                    
+                    }               
+                    if(admlevel==1){      
+                        var overlay = L.geoJson(geom,{
+                                style:{
+                                    fillColor: "#000000",
+                                    color: 'grey',
+                                    weight: 2,
+                                    opacity: 1,
+                                    fillOpacity: 0,
+                                    class:'adm'+admlevel
+                                },
+                                onEachFeature: onEachFeatureADM1
+                        });
+                        overlay.addTo(map);                      
+                        var whereDimension = cf.dimension(function(d,i){return d['#adm1+code']; });
+                        var newData = whereDimension.filter(filters[0]).top(Infinity);
+                        var newGeom = filterGeom(adm2_geom,filters[0],6);
+                        overlay1 = overlay;
+                        admlevel=2;
+                        generate3WComponent(newData,newGeom,map,'#adm2+code');
+                    } else if(admlevel==2){
+                        /*var overlay = L.geoJson(geom,{
+                                style:{
+                                    fillColor: "#000000",
+                                    color: 'grey',
+                                    weight: 2,
+                                    opacity: 1,
+                                    fillOpacity: 0,
+                                    class:'adm'+admlevel
+                                },
+                                onEachFeature: onEachFeatureADM2
+                        }); */                   
+                        var whereDimension = cf.dimension(function(d,i){return d['#adm2+code']; });
+                        var newData = whereDimension.filter(filters[0]).top(Infinity);
+                        var newGeom = filterGeom(adm3_geom,filters[0],9);
+                        /*overlay.addTo(map); 
+                        overlay2 = overlay;*/
+                        admlevel=3;
+                        generate3WComponent(newData,newGeom,map,'#adm3+code');
+                    }
                 }
             });
             /*.on("renderlet",(function(e){
@@ -267,6 +300,7 @@ function filterGeom(geom,filter,length){
     });
     newgeom.features = newFeatures;
     updateUntrained(filter);
+    $('#area_title').html('<h3>'+lookup[filter]+'</h3>');
     return newgeom;
 }
 
@@ -276,17 +310,20 @@ function zoomToGeom(geom){
 }
     
 function genLookupToName(){
-    var lookup = {};
+
     adm1_geom.features.forEach(function(f){
         lookup[f.properties.CODE] = f.properties.NAME;
+        lookupSmall[f.properties.CODE] = f.properties.NAME;
     });
     adm2_geom.features.forEach(function(f){
         lookup[f.properties.CODE] = f.properties.ADM1_NAME + ', ' + f.properties.NAME;
+        lookupSmall[f.properties.CODE] = f.properties.NAME;
     });
     adm3_geom.features.forEach(function(f){
         lookup[f.properties.CODE] = f.properties.ADM1_NAME + ', ' + f.properties.ADM2_NAME + ', ' + f.properties.NAME;
+        lookupSmall[f.properties.CODE] = f.properties.NAME;
     });
-    return lookup;    
+    
 }
 
 function hxlProxyToJSON(input){
@@ -349,11 +386,18 @@ var geoLayer = '';
 var overlay1 = '';
 var overlay2 = '';
 var admlevel=1;
-
+var color = 'steelblue';
 
 var adm1_geom = topojson.feature(gui_adm1,gui_adm1.objects.gui_adm1);
 var adm2_geom = topojson.feature(gui_adm2,gui_adm2.objects.gui_adm2);
 var adm3_geom = topojson.feature(gui_adm3,gui_adm3.objects.gui_adm3);
-var lookup = genLookupToName();
-
+var lookup = {};
+var lookupSmall = {};
+genLookupToName();
+$('#reinit').click(function(){
+        map.removeLayer(dcGeoLayer);
+        map.removeLayer(overlay1);
+        admlevel=1; 
+        generate3WComponent(data,adm1_geom,map,'#adm1+code');
+    });
 initDash();
